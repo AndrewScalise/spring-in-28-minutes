58d94648194ee90ac7da4401178d5658fbaf05f9
Andrew Scalise <paradigmcomplex@gmail.com>
2018-05-15 21:02:55 -0500

    loose coupling

---
+++ src/main/java/com/in28minutes/spring/basics/springin5steps/BinarySearchImpl.java
@@ -0,0 +1,14 @@
+package com.in28minutes.spring.basics.springin5steps;
+
+public class BinarySearchImpl {
+
+    public int binarySearch(int[] numbers, int numberToSearchFor){
+
+        BubbleSortAlgorithm bubbleSortAlgorithm = new BubbleSortAlgorithm();
+        int[] sortedNumbers = bubbleSortAlgorithm.sort(numbers);
+        return 3;
+    }
+    //Sorting an array
+    //Search the array
+    //Return the result
+}
